ba0531c46494706470ad2f02c9dc9bf9810363ed
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-11-26 17:30:35 +0000

    init comit

---
+++ src/white_belt/week1/01/assignment1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+int main(int argc, char * argv[]) {
+
+    int a;
+    int b;
+
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+
+    return 0;
+}
