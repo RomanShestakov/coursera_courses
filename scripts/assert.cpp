4769e5953d16f4d2ed54082aa61b4b03678d0100
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-11-27 19:08:27 +0000

    ass assert

---
+++ scripts/assert.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+
+int main(int argc, char* argv[]) {
+
+    if( argc != 2 ) {
+        std::cout << "Usage: assert <expectedResult>" << std::endl;
+        exit( 1 );
+    }
+
+    std::string expected = argv[ 1 ];
+    std::string result;
+
+    std::getline(std::cin, result );
+
+    if( result != expected ) {
+        std::cout << "Found:" << result << " expected:" << expected << " FAIL" << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << "Found:" << result << " expected:" << expected << " PASSED" << std::endl;
+    }
+
+    return 0;
+}
