5ee235e582bcc8695ef978edd301f46474df1c4e
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-11-27 11:48:47 +0000

    add 02 assingment

---
+++ src/white_belt/week1/02/assignment2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    std:vector< std::string > v;
+    std::string str;
+
+    while( std::getline( std::cin, str, ' ' ) ) {
+        v.push_back( str );
+    }
+
+    auto it = std::min_element( v.begin(), v.end() );
+
+    std::cout << "Min element: " << *it << std::endl;
+
+}
