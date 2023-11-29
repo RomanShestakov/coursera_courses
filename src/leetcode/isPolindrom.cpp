c7843ecf7c4443b1df96efbb0f4bcfef1feb6e47
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-11-29 11:39:06 +0000

    add isPolindrom

---
+++ src/leetcode/isPolindrom.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include <string>
+
+bool isPolindrom( const std::string& str ) {
+    int l = 0;
+    int r = str.size() - 1;
+
+    while( l <= r ) {
+        if( str[ l++ ] != str[ r-- ] ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main() {
+    std::string str;
+
+    std::cin >> str;
+
+    std::cout << "IsPolindrom:" << std::boolalpha << isPolindrom( str ) << std::endl;
+
+    return 0;
+}
