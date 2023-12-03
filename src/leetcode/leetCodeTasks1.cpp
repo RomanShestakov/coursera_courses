dc6b97ef09bcfa214a8aa5697b91218797aefe61
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-12-03 09:50:15 +0000

    add reverseStr

--- src/leetcode/leetCodeTasks1.cpp
+++ src/leetcode/leetCodeTasks1.cpp
@@ -484,6 +484,105 @@ int waysToSplitArray(std::vector<int>& nums ) {
 // }
 
 
+
+// ou are given a 0-indexed array nums of n integers, and an integer k.
+
+// The k-radius average for a subarray of nums centered at some index i with the radius k is the average of all elements in nums between the indices i - k and i + k (inclusive). If there are less than k elements before or after the index i, then the k-radius average is -1.
+
+// Build and return an array avgs of length n where avgs[i] is the k-radius average for the subarray centered at index i.
+
+// The average of x elements is the sum of the x elements divided by x, using integer division. The integer division truncates toward zero, which means losing its fractional part.
+
+// For example, the average of four elements 2, 3, 1, and 5 is (2 + 3 + 1 + 5) / 4 = 11 / 4 = 2.75, which truncates to 2.
+
+
+// Example 1:
+
+
+// Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
+// Output: [-1,-1,-1,5,4,4,-1,-1,-1]
+// Explanation:
+// - avg[0], avg[1], and avg[2] are -1 because there are less than k elements before each index.
+// - The sum of the subarray centered at index 3 with radius 3 is: 7 + 4 + 3 + 9 + 1 + 8 + 5 = 37.
+//   Using integer division, avg[3] = 37 / 7 = 5.
+// - For the subarray centered at index 4, avg[4] = (4 + 3 + 9 + 1 + 8 + 5 + 2) / 7 = 4.
+// - For the subarray centered at index 5, avg[5] = (3 + 9 + 1 + 8 + 5 + 2 + 6) / 7 = 4.
+// - avg[6], avg[7], and avg[8] are -1 because there are less than k elements after each index.
+// Example 2:
+
+// Input: nums = [100000], k = 0
+// Output: [100000]
+// Explanation:
+// - The sum of the subarray centered at index 0 with radius 0 is: 100000.
+//   avg[0] = 100000 / 1 = 100000.
+// Example 3:
+
+// Input: nums = [8], k = 100000
+// Output: [-1]
+// Explanation:
+// - avg[0] is -1 because there are less than k elements before and after index 0.
+
+// vector<int> getAverages(vector<int>& nums, int k) {
+// }
+
+
+
+
+
+// Given a string s, reverse the astring according to the following rules:
+
+// All the characters that are not English letters remain in the same position.
+// All the English letters (lowercase or uppercase) should be reversed.
+// Return s after reversing it.
+
+
+
+// Example 1:
+
+// Input: s = "ab-cd"
+// Output: "dc-ba"
+// Example 2:
+
+// Input: s = "a-bC-dEf-ghIj"
+// Output: "j-Ih-gfE-dCba"
+// Example 3:
+
+// Input: s = "Test1ng-Leet=code-Q!"
+// Output: "Qedo1ct-eeLg=ntse-T!"
+std::string reverseStr( std::string s ) {
+
+    int l = 0;
+    int r = s.size() - 1;
+
+
+    auto isLetter = [](char c){ return ( c >= 'a' && c<= 'z') || (c>= 'A' && c <= 'Z'); };
+
+    while( l < r ) {
+
+        while( !isLetter( s[ l ] ) && l < r ) {
+            l++;
+        }
+
+
+        while( !isLetter( s[ r ] ) && r > l ) {
+            r--;
+        }
+
+        char tmp = s[ l ];
+        s[ l ] = s[ r ];
+        s[ r ] = tmp;
+
+        //std::cout << s[ l ] << isLetter( s[ l ] ) << std::endl;
+
+        l++;
+        r--;
+    }
+
+    return s;
+}
+
+
+
 using namespace leetcode;
 TEST(twoSum, case1) {
     std::vector<int> v = {2,7,11,15};
@@ -603,3 +702,23 @@ TEST(waystoSplitarray, case1) {
 //     int expected = 1;
 //     EXPECT_EQ( expected, minStartValue( v ) );
 // }
+
+
+// Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
+// Output: [-1,-1,-1,5,4,4,-1,-1,-1]
+// TEST(getAverages, case2) {
+//     std::vector<int> v = {7,4,3,9,1,8,5,2,6};
+//     std::vector< int > expected = {-1,-1,-1,5,4,4,-1,-1,-1};
+//     EXPECT_EQ( expected, getAverages( v, 3 ) );
+// }
+
+
+
+
+// Input: s = "ab-cd"
+// Output: "dc-ba"
+TEST(reverseStr, case1) {
+    std::string s = "ab-cd";
+    std::string expected = "dc-ba";
+    EXPECT_EQ( expected, reverseStr( s ) );
+}
