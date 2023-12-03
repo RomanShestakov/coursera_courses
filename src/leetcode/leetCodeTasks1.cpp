c28bf42ff5e10d86954cad29eab3f3401dcd3364
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-12-03 21:21:39 +0000

    add max substr

--- src/leetcode/leetCodeTasks1.cpp
+++ src/leetcode/leetCodeTasks1.cpp
@@ -743,6 +743,62 @@ int maxVowels(string s, int k) {
     return a;
 }
 
+// You are given two strings s and t of the same length and an integer maxCost.
+
+// You want to change s to t. Changing the ith character of s to ith character of t costs |s[i] - t[i]| (i.e., the absolute difference between the ASCII values of the characters).
+
+// Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+
+
+
+// Example 1:
+
+// Input: s = "abcd", t = "bcdf", maxCost = 3
+// Output: 3
+// Explanation: "abc" of s can change to "bcd".
+// That costs 3, so the maximum length is 3.
+// Example 2:
+
+// Input: s = "abcd", t = "cdef", maxCost = 3
+// Output: 1
+// Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
+// Example 3:
+
+// Input: s = "abcd", t = "acde", maxCost = 0
+// Output: 1
+// Explanation: You cannot make any change, so the maximum length is 1.
+
+int equalSubstring(string s, string t, int maxCost) {
+
+    // calc diffs between s and t
+
+    //iter over diff to collect sums
+
+    // collect results
+
+    std::vector< int> diffs;
+
+    for( int i = 0; i< s.size(); i++ ) {
+        diffs.push_back( abs( s[ i ] - t[ i ]));
+    }
+
+    int curr = 0;
+    int a = 0;
+    int l = 0;
+    for( int r = 0; r < diffs.size(); r++ ) {
+        curr += diffs[ r ];
+
+        while( curr > maxCost && l <= r ) {
+            curr -= diffs[ l++ ];
+        }
+
+        a = max( r -l +1, a );
+
+    }
+
+    return a;
+
+}
 
 using namespace leetcode;
 TEST(twoSum, case1) {
@@ -910,4 +966,12 @@ TEST(maxVowels, case1) {
     std::string s = "abciiidef";
     int expected = 3;
     EXPECT_EQ( expected, maxVowels( s, 3 ) );
+}
+
+
+TEST(equalSubstring, case1) {
+    std::string s = "abcd";
+    std::string t = "bcdf";
+    int expected = 3;
+    EXPECT_EQ( expected, equalSubstring( s, t, 3 ) );
 }
