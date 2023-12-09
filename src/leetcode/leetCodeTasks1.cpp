dd0d783c3c89ea7623cdcb507a97f5af0276c330
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-12-09 20:28:35 +0000

    add largerst number

--- src/leetcode/leetCodeTasks1.cpp
+++ src/leetcode/leetCodeTasks1.cpp
@@ -1216,6 +1216,34 @@ vector<vector<int>> findWinners(vector<vector<int>>& matches) {
 }
 
 
+
+// Given an integer array nums, return the largest integer that only occurs once. If no integer occurs once, return -1.
+// Example 1:
+
+// Input: nums = [5,7,3,9,4,9,8,3,1]
+// Output: 8
+// Explanation: The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it is the answer.
+// Example 2:
+
+// Input: nums = [9,9,8,8]
+// Output: -1
+// Explanation: There is no number that occurs only once.
+int largestUniqueNumber(vector<int>& nums) {
+    std::map<int, int> m;
+
+    for( auto n : nums ) {
+        m[ n ]++;
+    }
+
+    for( auto it = m.rbegin(); it != m.rend(); it++ ) {
+        if( it -> second == 1 ) {
+            return it -> first;
+        }
+    }
+
+    return -1;
+}
+
 using namespace leetcode;
 
 TEST(twoSum, case1) {
@@ -1491,4 +1519,22 @@ TEST(findWinners, case1) {
     auto result = findWinners( v );
     vector<vector<int>> expected = {{1,2,10},{4,5,7,8}};
     EXPECT_EQ( expected, result );
+}
+
+
+
+// Input: nums = [5,7,3,9,4,9,8,3,1]
+// Output: 8
+// Explanation: The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it is the answer.
+// Example 2:
+
+// Input: nums = [9,9,8,8]
+// Output: -1
+// Explanation: There is no number that occurs only once.
+//int largestUniqueNumber(vector<int>& nums) {
+
+TEST(largestUniqueNumber, case1) {
+    std::vector<int> v = {5,7,3,9,4,9,8,3,1};
+    int expected = 8;
+    EXPECT_EQ( expected, largestUniqueNumber( v ) );
 }
