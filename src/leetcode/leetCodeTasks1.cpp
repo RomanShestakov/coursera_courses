41a4a8334375e645120019f5a9b2cf0a8c50223a
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-12-09 20:17:09 +0000

    add winplayers

--- src/leetcode/leetCodeTasks1.cpp
+++ src/leetcode/leetCodeTasks1.cpp
@@ -1151,6 +1151,70 @@ int numberOfSubarrays(vector<int>& nums, int k) {
 
 
 
+// You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+
+// Return a list answer of size 2 where:
+
+// answer[0] is a list of all players that have not lost any matches.
+// answer[1] is a list of all players that have lost exactly one match.
+// The values in the two lists should be returned in increasing order.
+
+// Note:
+
+// You should only consider the players that have played at least one match.
+// The testcases will be generated such that no two matches will have the same outcome.
+
+
+// Example 1:
+
+// Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+// Output: [[1,2,10],[4,5,7,8]]
+// Explanation:
+// Players 1, 2, and 10 have not lost any matches.
+// Players 4, 5, 7, and 8 each have lost one match.
+// Players 3, 6, and 9 each have lost two matches.
+// Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+// Example 2:
+
+// Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+// Output: [[1,2,5,6],[]]
+// Explanation:
+// Players 1, 2, 5, and 6 have not lost any matches.
+// Players 3 and 4 each have lost two matches.
+// Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+
+vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+    std::unordered_map<int, int > m;
+
+    for( auto el: matches ) {
+        auto winner = el[ 0 ];
+        auto looser = el[ 1 ];
+
+        if( m.find(winner) == m.end()) {
+            m[ winner ] = 0;
+        }
+
+        m[ looser ]++;
+    }
+
+    std::vector<int> a;
+    std::vector<int> b;
+
+    for( auto[ pl, sc]  : m ) {
+        if( sc == 0 ) {
+            a.push_back( pl );
+        }
+        else if( sc == 1 ){
+            b.push_back( pl );
+        }
+    }
+
+    std::sort( a.begin(), a.end());
+    std::sort( b.begin(), b.end());
+
+    return { a, b };
+}
+
 
 using namespace leetcode;
 
@@ -1415,4 +1479,16 @@ TEST(numberOfSubarrays, case3) {
     std::vector<int> v = {2,2,2,1,2,2,1,2,2,2};
     int expected = 16;
     EXPECT_EQ( expected, numberOfSubarrays( v, 2 ) );
+}
+
+
+
+// Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+// Output: [[1,2,10],[4,5,7,8]]
+//vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+TEST(findWinners, case1) {
+    vector<vector<int>> v = {{1,3},{2,3},{3,6},{5,6},{5,7},{4,5},{4,8},{4,9},{10,4},{10,9}};
+    auto result = findWinners( v );
+    vector<vector<int>> expected = {{1,2,10},{4,5,7,8}};
+    EXPECT_EQ( expected, result );
 }
