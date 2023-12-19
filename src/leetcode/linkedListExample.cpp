ac7985ebc796ee06e1af596d4b0abbd60fb88310
RomanShestakov <RomanShestakov@yahoo.co.uk>
2023-12-19 17:49:45 +0000

    update example

--- src/leetcode/linkedListExample.cpp
+++ src/leetcode/linkedListExample.cpp
@@ -1,9 +1,34 @@
 #include<iostream>
+#include<memory>
 
 struct LinkedNode {
+    bool verbose_ = false;
     int val_;
-    LinkedNode *next_;
+    LinkedNode *next_ = nullptr;
+    LinkedNode *prev_ = nullptr;
     LinkedNode( int val ) : val_( val ) {}
+
+    ~LinkedNode() {
+        if( verbose_ ) {
+            std::cout << "Removing:" << val_;
+        }
+    }
+
+    LinkedNode* addNode( LinkedNode* node ) {
+        node -> next_ = next_;
+        next_ = node;
+        node -> prev_ = this;
+        return node;
+    }
+
+    LinkedNode* deleteNode() {
+
+        if( prev_ ) prev_ -> next_ = next_;
+        if( next_ ) next_ -> prev_ = prev_;
+//        delete this;
+        return prev_;
+    }
+
 };
 
 int main() {
@@ -11,20 +36,28 @@ int main() {
     auto a = new LinkedNode( 1 );
     auto b = new LinkedNode( 2 );
     auto c = new LinkedNode( 3 );
+    auto d = new LinkedNode( 4 );
+    auto e = new LinkedNode( 5 );
+
     auto head = a;
+    head -> addNode( b ) -> addNode( c ) -> addNode( d ) -> addNode( e );
 
-    a -> next_ = b;
-    b -> next_ = c;
-    c -> next_ = nullptr;
-
-    while( head ) {
-        std::cout << head -> val_ << " " << std::endl;
-        head = head -> next_;
+    auto fast = head;
+    auto slow = head;
+    while( fast != nullptr && fast -> next_ != nullptr ) {
+        std::cout << head -> val_ << " ";
+        slow = slow -> next_;
+        fast = fast -> next_ -> next_;
     }
+
+    std::cout << "fast:" << fast -> val_ << " slow:" << slow -> val_ << std::endl;
+
 
     delete a;
     delete b;
     delete c;
+    delete d;
+    delete e;
 
     return 0;
 }
