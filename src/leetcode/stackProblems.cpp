f9b0dafa9cd709ba5ed08682be81a7d2424ed7b7
RomanShestakov <RomanShestakov@yahoo.co.uk>
2024-01-09 19:20:43 +0000

    refactor into libs

---
+++ src/leetcode/stackProblems.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<stack>
+#include"stackProblems.h"
+
+namespace leetcode {
+
+
+    std::string simplifyPath( std::string path){
+        return "";
+    }
+
+
+}
---
+++ src/leetcode/stackProblemsTests.cpp
@@ -0,0 +1,31 @@
+
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <vector>
+#include <unordered_map>
+#include <limits>
+#include <memory>
+#include "stackProblems.h"
+
+namespace leetcode {
+
+
+    // TEST(hasCycle, case1) {
+
+    //     auto a = std::make_unique< ListNode >( 3 );
+    //     auto b = std::make_unique< ListNode >( 2 );
+    //     auto c = std::make_unique< ListNode >( 0 );
+    //     auto d = std::make_unique< ListNode >( -4 );
+
+    //     auto head = a.get();
+    //     a -> addNode( b.get() ) ->
+    //          addNode( c.get() ) ->
+    //          addNode( d.get() );
+    //     d -> next = b.get();
+
+    //     EXPECT_TRUE( hasCycle( head ) );
+    //     EXPECT_TRUE( hasCycle1( head ) );
+    // }
+}
