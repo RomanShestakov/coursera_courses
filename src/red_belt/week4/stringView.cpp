6ea941989db5a5ecef8c0bcf16fc807d89f96684
RomanShestakov <RomanShestakov@yahoo.co.uk>
2024-01-10 20:30:53 +0000

    add string_view example

--- src/red_belt/week4/stringView.cpp
+++ src/red_belt/week4/stringView.cpp
@@ -5,9 +5,26 @@
 namespace red_belt {
 
     std::vector<std::string_view> split( std::string_view s ) {
-        std::vector< std::string_view > result;
+        std::vector< std::string_view > v;
 
-        return result;
+        size_t pos = 0;
+        const size_t pos_end = s.npos;
+
+        while( true ) {
+            size_t space = s.find( " ", pos );
+            v.push_back(
+                ( pos == pos_end
+                  ? s.substr( pos )
+                  : s.substr( pos, space - pos ) ) );
+
+            if( space == pos_end ) {
+                break;
+            } else {
+                pos = space + 1;
+            }
+        }
+
+        return v;
 
     }
 }
