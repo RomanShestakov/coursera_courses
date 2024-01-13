8283979527812b0a8b76c1da5e6bca81b7268ba0
RomanShestakov <RomanShestakov@yahoo.co.uk>
2024-01-13 22:23:23 +0000

    add some example for 05

---
+++ src/cpp_lectures/05/method_annotation.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+// https://www.youtube.com/watch?v=_mLDaU4wSOo
+// 39::00
+
+struct S {
+    int n = 10;
+    int& foo() & { return n; };
+    int& foo() && { return n; };
+};
+
+struct SS {
+    int n = 10;
+    int& foo() & { return n; };
+};
+
+int main() {
+
+    extern S bar();
+    S x {};
+
+    std::cout << "foo()&: " << x.foo() << " foo()&&: " << bar().foo() << std::endl;
+
+    // https://www.youtube.com/watch?v=_mLDaU4wSOo
+    // 41::00
+    //auto d = SS{}.foo(); // compilation err as SS is rvalue and foo() can only be called for lvalue
+    return 0;
+}
---
+++ src/cpp_lectures/05/rvalue.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+// https://www.youtube.com/watch?v=_mLDaU4wSOo
+// 31:00
+
+int main() {
+
+    int x = 4;
+    int&& y = x + 1;
+
+    std::cout << "y:" << y << " x:" << x << std::endl;
+    std::cout << "&y:" << &y << " &x:" << &x << std::endl;
+
+    int&& z = std::move( x );
+
+    z = z + 1;
+
+    std::cout << "z:" << y << " x:" << x << std::endl;
+    std::cout << "&z:" << &z << " &x:" << &x << std::endl;
+
+
+
+    // cross ref
+    //int&& b = x; // fail
+    // int &v = x + 1; // fail
+
+    const int& d = x + 1; //OK - const ref prolongs the live of object
+
+    //int &&e = y; // fail
+    int& g = y;
+
+    return 0;
+}
